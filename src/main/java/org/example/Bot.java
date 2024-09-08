0bdf598fe72400201e564a4f68ef42de8d3677eb
starfirs <starfirs@student.21-school.ru>
2024-09-09 00:36:00 +0300

    тело телеграм бота написано, нужно докрутить и добавить функционал и.т.п

---
+++ src/main/java/org/example/Bot.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public Bot(String botToken) {
+        super(botToken);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "Java test bot";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.getMessage().getText().equals("/start")) {
+            try {
+                sendMessage(update.getMessage().getFrom().getId(), "Приветствую вас мой дорогой друг! это тестовый телеграм бот!", "Button 1", "Button 2");
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    // method 1
+    public void sendMessage(long id, String text) throws TelegramApiException {
+        SendMessage sendMessage = SendMessage.builder()
+                .text(text)
+                .chatId(id)
+                .build();
+        sendApiMethod(sendMessage);
+    }
+    // method2 перегрузка
+    public void sendMessage(long id, String text, String nameButton1, String nameButton2) throws TelegramApiException {
+        InlineKeyboardMarkup keyboardMarkup = sendCallbackQuery(nameButton1, nameButton2);
+        SendMessage sendMessage = SendMessage.builder()
+                .chatId(id)
+                .text(text)
+                .replyMarkup(keyboardMarkup)
+                .build();
+        sendApiMethod(sendMessage);
+    }
+
+    public InlineKeyboardMarkup sendCallbackQuery(String nameButton1, String nameButton2) {
+        InlineKeyboardButton button1 = InlineKeyboardButton.builder()
+                .text(nameButton1).callbackData("test1")
+                .build();
+        InlineKeyboardButton button2 = InlineKeyboardButton.builder()
+                .text(nameButton2).callbackData("test2")
+                .build();
+
+        InlineKeyboardMarkup keyboard = InlineKeyboardMarkup.builder()
+                .keyboardRow(List.of(button1, button2))
+                .build();
+        return keyboard;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        Bot bot = new Bot(""); // токен бота в телеграме в избранном 09.09.2024 00:30
+        botsApi.registerBot(bot);
+    }
+}
